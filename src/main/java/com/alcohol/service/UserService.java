0daaddd9f5998ede15720a7b45f160b10bfaa5ff
m17696114034 <m17696114034@163.com>
2018-07-05 16:16:34 +0800

    张鑫鹏  后台用户

--- src/main/java/com/alcohol/service/UserService.java
+++ src/main/java/com/alcohol/service/UserService.java
@@ -1,7 +1,30 @@
 package com.alcohol.service;
 
+import com.alcohol.pojo.User;
 import com.alcohol.pojo.Useraccount;
+
+import java.util.List;
+import java.util.Map;
 
 public interface UserService {
     public boolean ZhuCe(Useraccount useraccount);
+    /**
+     * 查询所有用户
+     * @return
+     */
+    public List<User> listUser(Integer pageNum, Integer pageSize);
+    /**
+     * 按昵称查找
+     * @param nickName
+     * @return
+     */
+    public List<User> getNickNameOne(String nickName);
+
+    /**
+     * 禁用启用用户
+     * @param userId
+     * @param status
+     * @return
+     */
+    public int updStatus(Long userId,Long status);
 }
