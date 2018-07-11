5cbc1ac27908a608fa4cb666df7f005ad2e1e1f1
wl0927 <1226682591@qq.com>
2018-07-11 11:40:03 +0800

    王磊修改二级分类三级分类全部

--- src/main/java/com/alcohol/controller/CategoryController.java
+++ src/main/java/com/alcohol/controller/CategoryController.java
@@ -51,55 +51,6 @@ public class CategoryController {
         categoryoneList=categoryOneService.getCategoryOneInfo();
         return JSON.toJSONString(categoryoneList);
     }
-
-   /* *//**
-     * 根据一级所选分类查询所有的一级分类下的二级分类
-     * @param map
-     * @return
-     *//*
-    @RequestMapping(value="listwo",produces = "text/html;charset=utf-8")
-    @ResponseBody
-    public String listwo(Map<String,Object> map){
-        categorytwoList=categoryTwoService.getCategoryTwoInfo(map);
-        return JSON.toJSONString(categorytwoList);
-    }*/
-
-    /**
-     * 根据二级所选分类查询所有的二级下的三级分类
-     * @param map
-     * @return
-     */
-    @RequestMapping(value="listThree",produces = "text/html;charset=utf-8")
-    @ResponseBody
-    public String listThree(Map<String,Object> map){
-        categorythreeList=categoryThreeService.getCategoryThreeInfo(map);
-        return JSON.toJSONString(categorythreeList);
-    }
-
-    /**
-     * 根据所选的三级分类查询对应的属性
-     * @param map
-     * @return
-     */
-    @RequestMapping(value="listTypeName",produces = "text/html;charset=utf-8")
-    @ResponseBody
-    public String listTypeName(Map<String,Object> map){
-        listTypeName=typeNameService.getTypeName(map);
-        return JSON.toJSONString(listTypeName);
-    }
-
-    /**
-     * 根据属性查询出对应的属性值
-     * @param map
-     * @return
-     */
-    @RequestMapping(value="listTypeValue",produces = "text/html;charset=utf-8")
-    @ResponseBody
-    public String listTypeValue(Map<String,Object> map){
-        listTypeValue=typeValueService.getTypeValue(map);
-        return JSON.toJSONString(listTypeValue);
-    }
-
     /**
      * 得到属性和属性值
      * @param request
@@ -110,17 +61,5 @@ public class CategoryController {
     public String getCategorythreeById(HttpServletRequest request){
         Integer id=(Integer) request.getSession().getAttribute("categoryId");
         return JSON.toJSONString(categoryThreeService.getCategorythreeById(id));
-    }
-
-
-    /**
-     * 查询三级上的二级
-     * @param parentId
-     * @return
-     */
-    @RequestMapping(value="/getCategorytwoBythreeId",produces = "text/html;charset=utf-8")
-    @ResponseBody
-    public String getCategorytwoBythreeId(Integer parentId){
-        return JSON.toJSONString(categoryTwoService.getCategorytwoBythreeId(parentId));
     }
 }
