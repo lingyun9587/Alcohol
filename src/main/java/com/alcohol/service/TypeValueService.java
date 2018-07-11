2fff37e6688d72f5f98159062572d134592e90be
wl0927 <1226682591@qq.com>
2018-07-11 11:12:48 +0800

    王磊属性、属性值、service

--- src/main/java/com/alcohol/service/TypeValueService.java
+++ src/main/java/com/alcohol/service/TypeValueService.java
@@ -1,6 +1,7 @@
 package com.alcohol.service;
 
 import com.alcohol.pojo.Typevalue;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Map;
@@ -13,6 +14,12 @@ public interface TypeValueService {
      * @return
      */
     List<Typevalue> getTypeValue(Map<String, Object> map);
+    /**
+     * 根据属性查询对应的属性值
+     * @param tv
+     * @return
+     */
+    List<Typevalue> getTypeValuefy(Typevalue tv, @RequestParam("pageNum") Integer pageNum, @RequestParam("pageSize") Integer pageSize);
 
     /**
      * 新增属性值
@@ -22,23 +29,29 @@ public interface TypeValueService {
     int addTypeValue(Typevalue tn);
 
     /**
+     * 判断修改的属性值是否存在
+     * @param tv
+     * @return
+     */
+    int updissel(Typevalue tv);
+
+    /**
      * 修改属性值
      * @param tn
      * @return
      */
     int modifyTypeValue(Typevalue tn);
+    /**
+     * 判断删除的属性值下是否有商品
+     * @param tv
+     * @return
+     */
+    int delisshop(Typevalue tv);
 
     /**
      * 删除属性值
-     * @param typeValueId
+     * @param tv
      * @return
      */
-    int delTypeValue(Long typeValueId);
-
-    /**
-     * 根据Id获取值
-     * @param typeValueId
-     * @return
-     */
-    Typevalue selIdType(Long typeValueId);
+    int delTypeValue(Typevalue tv);
 }
