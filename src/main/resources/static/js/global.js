76ad8cbeddb73a85db5ae398493781c115cb74ea
lingyun9587 <834218828@qq.com>
2018-07-18 08:20:08 +0800

    个人资料

--- src/main/resources/static/js/global.js
+++ src/main/resources/static/js/global.js
@@ -173,7 +173,7 @@ $(function(){
 
 				$(".tab-header .inner .pull-right a").eq(0).html(data.user.membershipName);
                 $(".tab-header .inner .pull-right a").eq(0).attr("href","javascript:void(0)");
-
+                $(".tab-header .inner .pull-right a").eq(1).hide();
 			}
 		},
 		error:function(){
